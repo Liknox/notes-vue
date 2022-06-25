6f2202ce409e33b6d4e2028e1fef1bf7034dc4da
Nazar Koval <nazarkoval04@gmail.com>
2022-06-26 02:50:53 +0300

    injected vue-cli

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+	<div class="wrappper">
+		<div class="wrapper-content">
+			<section>
+				<div class="container">
+					<h1>{{ title }}</h1>
+					<div class="message" v-if="message">
+						<p>{{ message }}</p>
+					</div>
+					<!-- new note -->
+					<div class="new-note">
+						<input v-model="note.title" type="text" />
+						<textarea v-model="note.descr"></textarea>
+						<button @click="addNote">New note</button>
+					</div>
+
+					<!-- note list -->
+					<div class="notes">
+						<div class="note" v-for="(note, index) in notes" :key="index">
+							<div class="note-header">
+								<p>{{ note.title }}</p>
+							</div>
+							<div class="note-body">
+								<p>{{ note.descr }}</p>
+								<span> {{ note.date }} </span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: "Notes App",
+			message: false,
+			note: {
+				title: "",
+				descr: "",
+			},
+			notes: [
+				{
+					title: "First Project",
+					descr: "Description for first note",
+					date: new Date(Date.now()).toLocaleString(),
+				},
+				{
+					title: "Second Project",
+					descr: "Description for Second note",
+					date: new Date(Date.now()).toLocaleString(),
+				},
+				{
+					title: "Third Project",
+					descr: "Description for Third note",
+					date: new Date(Date.now()).toLocaleString(),
+				},
+			],
+		}
+	},
+	methods: {
+		addNote() {
+			// console.log(this.note)
+			let {title, descr} = this.note
+
+			if (title === "") {
+				this.message = "title can't be empty"
+				return false
+			}
+			this.notes.push({
+				// title: this.note.title
+				// descr: this.note.descr
+				title,
+				descr,
+				date: new Date(Date.now()).toLocaleString(),
+			})
+			this.message = null
+			this.note.title = ""
+			this.note.descr = ""
+		},
+	},
+}
+</script>
+
+<style></style>
